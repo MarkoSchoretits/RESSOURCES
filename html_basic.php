7dc50f30525e344e0bdb68ef22939ff8a02ff061
MarkoSchoretits <marko@schoretits.com>
2023-01-24 15:47:31 +0100

    add ex01, html_basic

--- html_basic.php
+++ html_basic.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inkognito Ressources</title>
-
+    
     <style>
         * {
             margin: 0;
@@ -28,7 +28,18 @@
     </aside>
 
     <main>
-        Main Content
+        <article>
+            <h1>1. HTML Basic</h1>
+            <h2>1.2. Some Basic Tags</h2>
+            <p>
+                This is <b>bold</b> Text.<br>
+                This is <strong>strong</strong> Content.<br>
+                This is <i>italic</i> Text.<br>
+                This is <em>emphasized</em> Content.<br>
+                Here comes some <sub>subscribed</sub> Text.<br>
+                And here some <sup>superscribed</sup> one.<br>
+            </p>
+        </article>
     </main>
 
     <footer>
